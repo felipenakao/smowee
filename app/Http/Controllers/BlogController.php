c54dab69282f0015788b249feb245c174fc77ee4
Caio Batista <cbatista.93@gmail.com>
2017-09-28 01:36:09 -0300

    categories info

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -14,7 +14,12 @@ class BlogController extends Controller
       // LISTAGEM DE TODOS POSTS
       // QUE PUDEREM SER VISTOS COM BASE NA DATA DE PUBLICACAO
       $posts = DB::table('posts')
-          ->whereDate('publish_date', '<=', Carbon::now())
+          ->join('posts_categories', 'posts.category_id', '=', 'posts_categories.id')
+          ->whereDate('posts.publish_date', '<=', Carbon::now())
+          ->select(
+            'posts.*',
+            'posts_categories.name as category_name',
+            'posts_categories.color as category_color')
           ->get();
       // INFINITY SCROLLER
       // RETORNAR VIEW COM TODOS OS POSTS
--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['title', 'cover', 'category_id', 'tags', 'publish_date', 'content', 'writer_id'];
+
+    public function category()
+    {
+      return $this->belongsToMany('App\Category', 'posts', 'category_id', 'id')
+    }
+
 }
