67a3aeeb9a39256567547fb2bcf08c865b584de0
Felipe Nakao <felipenakao@mini-de-felipe.home>
2017-10-21 23:55:15 -0200

    footer links & nav hover

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -53,9 +53,9 @@
       </div>
       <div class="col-md-2">
         <ul class="list-inline social-buttons">
-          <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-          <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-          <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+          <!--<li><a href="#"><i class="fa fa-twitter"></i></a></li>-->
+          <li><a target="_blank" href="https://www.facebook.com/SmoweeOficial/"><i class="fa fa-facebook"></i></a></li>
+          <li><a target="_blank" href="https://www.instagram.com/smowee/"><i class="fa fa-instagram"></i></a></li>
 
         </ul>
 
