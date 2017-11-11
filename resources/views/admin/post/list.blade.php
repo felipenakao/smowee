abbde326aa50fb456834fb6d7dab1d3aef34c73a
Caio Batista <cbatista.93@gmail.com>
2017-11-11 13:54:58 -0200

    list style, date format & insert button

--- resources/views/admin/post/list.blade.php
+++ resources/views/admin/post/list.blade.php
@@ -1,7 +1,7 @@
 @extends ('admin.master')
 {{-- TITULO; IMAGEM CAPA; CATEGORIA; TAG; CONTEUDO--}}
 @section ('content')
-  <h4>Matérias</h4>
+  <h4>Matérias <button class="btn btn-success">Nova matéria</button></h4>
   <hr>
   <table class="table table-striped table-admin">
     <tr>
@@ -15,11 +15,21 @@
     @foreach($posts as $post)
         <tr>
             <td>{{ $post->slug }}</td>
-            <td>{{ $post->tags }}</td>
+            {{--<td>{{ $post->tags }}</td>--}}
+            <td>
+                @foreach (explode(',', $post->tags) as $tag)
+                    <span class="label label-success">{{$tag}}</span>
+                @endforeach
+            </td>
             <td>{{ $post->views }}</td>
             <td>{{ $post->up_votes }}</td>
-            <td>{{ $post->publish_date}}</td>
-            <td></td>
+            <td>
+                {{ Carbon\Carbon::parse($post->publish_date)->format('d/m/Y') }}
+            </td>
+            <td class="text-right">
+                <i class="fa fa-pencil admin-post__action"></i>
+                <i class="fa fa-times admin-post__action"></i>
+            </td>
         </tr>
     @endforeach
   </table>
