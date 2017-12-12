9fd809264c71eb798c2e05f97bd5668f14caff49
Caio Batista <cbatista.93@gmail.com>
2017-12-12 11:18:42 -0200

    auth clube returning login view

--- app/Http/Middleware/AuthClube.php
+++ app/Http/Middleware/AuthClube.php
@@ -3,6 +3,8 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Http\Response;
 
 class AuthClube
 {
@@ -15,14 +17,21 @@ class AuthClube
      */
     public function handle($request, Closure $next)
     {
+        $response = $next($request);
+        $ogUrl = URL::current();
+        $ogType = 'website';
+        $ogTitle = 'Smowee 420 - Queimando Preconceitos';
+        $ogDescription = 'Participe do Clube Smowee e concorra a prêmios!';
+        $ogImage = 'http://smowee.com/img/about/about-bg.jpg';
+
         $user = $request->user();
         // verificar ano de nascimento
         // verificar acesso ao clube(flag clube)
-        dd($user->clube);
         if (!$user || $user->clube < 1){
-        
+            return Response(view('clube.login', compact(['ogUrl', 'ogType', 'ogTitle', 'ogDescription', 'ogImage'])));
+            dd($user);
         }
 
-        return $next($request);
+        return $response;
     }
 }
