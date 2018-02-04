9698fd1f0675288f3bb2256ef4d8e24ec02fe04c
Caio Batista <cbatista.93@gmail.com>
2018-02-03 22:03:33 -0200

    migration clube fields order fixed

--- database/migrations/2018_02_03_235432_create_clube_fields_to_users.php
+++ database/migrations/2018_02_03_235432_create_clube_fields_to_users.php
@@ -14,9 +14,9 @@ class CreateClubeFieldsToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('telephone')->nullable();
-            $table->string('city')->nullable();
-            $table->string('cpf')->nullable();
+            $table->string('telephone')->nullable()->after('role');
+            $table->string('city')->nullable()->after('telephone');
+            $table->string('cpf')->nullable()->after('cpf');
         });
     }
 
